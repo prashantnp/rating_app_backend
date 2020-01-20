68f4aa251592f75165a1e1818b999e9f09bd8648
prashant.np <pnp1808@gmail.com>
2020-01-20 20:59:22 +0530

    Load seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach(Rails.root.join('db', 'data', 'csv_data.csv'), headers: true) do |row|
+  row_hash = row.to_hash
+
+  mapping = Mapping.find_or_create_by(name: row_hash['Mapping'])
+  role = Role.find_or_create_by(name: row_hash['Role'])
+
+  question = Question.create(
+    priority: row_hash['Pri'],
+    question: row_hash['Question'],
+    teaming_stage: row_hash['Teaming Stages'],
+    appears_day: row_hash['Appears (Day)'],
+    frequency: row_hash['Frequency'],
+    question_type: row_hash['Type'],
+    required: row_hash['Required?'],
+    conditions: row_hash['Conditions'],
+    role: role,
+    mapping: mapping
+  )
+end
