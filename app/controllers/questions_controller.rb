b9c7957c06e777af870d300cfdcef5f03f3f6a35
prashant.np <pnp1808@gmail.com>
2020-01-20 23:58:25 +0530

    Add CORS & questions index API

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,5 @@
 class QuestionsController < ApplicationController
   def index
-    questions = Question.all.map { |question| question.details }
-    render status: 200, json: { questions: Question.all.map { |question| question.details }
+    render status: 200, json: { questions: Question.all.map {|question| question.details} }
   end
 end
